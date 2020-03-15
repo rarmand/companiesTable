f5695f5765ce030e5b07084fec670c2cf0cd7e71
rarmand <holik.alek@gmail.com>
2020-03-15 22:41:14 +0100

    axios gets data, table display on website working

--- src/components/CompanyTable/index.js
+++ src/components/CompanyTable/index.js
@@ -1,11 +1,50 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const pathUrl = `https://recruitment.hal.skygate.io/companies`;
 
 class CompanyTable extends Component {
+  state = {
+    companies: []
+  };
+
+  componentDidMount() {
+    axios.get(pathUrl).then(response => {
+      const data = response.data;
+      this.setState({ companies: data });
+    });
+  }
+
   render() {
+    const headers = {
+      id: "ID",
+      name: "Name",
+      city: "City",
+      totalIncome: "Total income"
+    };
+
     return (
       <div className="companyTable">
         <input type="text"></input>
-        <div className="companyTable__table"></div>
+        <div className="companyTable__table">
+          <table className="companyTable__table--table">
+            <tr className="companyTable__table--rowTitle">
+              {Object.values(headers).map(value => (
+                <th className="companyTable__table--cellHeader">{value}</th>
+              ))}
+            </tr>
+
+            {this.state.companies.map(company => (
+              <tr className="companyTable__table--row">
+                {Object.keys(headers).map(key => (
+                  <th className="companyTable__table--cellHeader">
+                    {company[key]}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </table>
+        </div>
       </div>
     );
   }
